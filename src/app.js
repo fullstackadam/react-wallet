0694089aa49b357778df2331cdfab0e39595a01b
Adam McMahon <mcmahonempire@gmail.com>
2017-12-06 15:15:30 -0700

    add: links

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -34,15 +34,28 @@ const NotFound = () => (
   </div>
 );
 
+const Header = () => (
+  <header>
+    <h1>Wallet</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  </header>
+);
+
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route path="/" component={Dashboard} exact={true} />
-      <Route path="/create" component={CreateExpense} />
-      <Route path="/edit" component={EditExpense} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFound} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={Dashboard} exact={true} />
+        <Route path="/create" component={CreateExpense} />
+        <Route path="/edit" component={EditExpense} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 
