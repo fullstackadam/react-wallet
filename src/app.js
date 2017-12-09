6bdd51667bf17d6c0d7ddfd5412d245c51b24225
Adam McMahon <mcmahonempire@gmail.com>
2017-12-09 11:24:04 -0700

    add: redux

--- src/app.js
+++ src/app.js
@@ -1,7 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configure';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 5000 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 30000 }));
+
+store.subscribe(() => {
+  const state = store.getState();
+
+  const visibleExpenses = getVisibleExpenses(
+    state.expenses,
+    state.filters
+  );
+
+  console.log(visibleExpenses);
+});
+
+store.dispatch(setTextFilter('water'));
+
+console.log(store.getState());
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
