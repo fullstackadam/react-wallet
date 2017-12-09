85f0be1348196da70768a27f90eaf1c50f4cf8c6
Adam McMahon <mcmahonempire@gmail.com>
2017-12-09 14:22:33 -0700

    add: remove expense to expense item component

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-export default ({amount, createdAt, description}) => (
+const ExpenseListItem = ({ id, amount, createdAt, description, dispatch }) => (
   <div>
     <h4>{amount} | {createdAt}</h4>
     <p>{description}</p>
+    <button onClick={() => {
+      dispatch(removeExpense({ id }))
+    }}>Remove</button>
   </div>
 );
+
+export default connect()(ExpenseListItem);
