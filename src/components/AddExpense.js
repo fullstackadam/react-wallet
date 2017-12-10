2c8bd6d9c8a592389c37dbd9b5fbe27d011750b1
Adam McMahon <mcmahonempire@gmail.com>
2017-12-09 17:00:27 -0700

    add: expense form

---
+++ src/components/AddExpense.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+
+
+const AddExpense = () => (
+  <div>
+    <h1>Add Expense</h1>
+    <ExpenseForm />
+  </div>
+);
+
+export default AddExpense;
--- src/components/CreateExpense.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CreateExpense = () => (
-  <div>
-    Create Expense
-  </div>
-);
-
-export default CreateExpense;
